a3008db1f72b7fb3f7250010f926f7b37a646503
Eduardo Brandao <eduardo.brandao.t1986@gmail.com>
2025-11-05 10:17:15 -0300

    fix/improve_swagger improve events swagger description

--- backend/src/events/const/publishEvent.dto.ts
+++ backend/src/events/const/publishEvent.dto.ts
@@ -2,11 +2,16 @@ import { ApiProperty } from '@nestjs/swagger';
 import { EventType } from './eventTypes';
 
 export class PublishEventDto {
-  @ApiProperty({ enum: EventType, example: EventType.PAYMENT_PROCESSED })
+  @ApiProperty({
+    enum: EventType,
+    description:
+      'Para testar payment.consumer: ORDER_CREATED e para stockConsumer: PAYMENT_PROCESSED',
+    example: EventType.PAYMENT_PROCESSED,
+  })
   type: EventType;
 
   @ApiProperty({
-    description: 'Conteúdo do evento (payload)',
+    description: 'Conteúdo do evento (nesse caso sempre será orderId: ${id})',
     example: {
       orderId: 1,
     },
